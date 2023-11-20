32193f66acc26283af81be2debc24711e98a9544
Odinei Ribeiro <odinei.ribeiro92@gmail.com>
2023-11-20 02:04:40 -0300

    Projeto criado

---
+++ lib/repo_reports.ex
@@ -0,0 +1,18 @@
+defmodule RepoReports do
+  @moduledoc """
+  Documentation for `RepoReports`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RepoReports.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/repo_reports_test.exs
@@ -0,0 +1,8 @@
+defmodule RepoReportsTest do
+  use ExUnit.Case
+  doctest RepoReports
+
+  test "greets the world" do
+    assert RepoReports.hello() == :world
+  end
+end
