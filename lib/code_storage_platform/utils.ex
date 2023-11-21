32c053560352ffb5f25e7c109795f29d076b490e
Odinei Ribeiro <odinei.ribeiro92@gmail.com>
2023-11-21 01:54:40 -0300

    Refatora CodeStoragePlatform.Utils

--- lib/code_storage_platform/utils.ex
+++ lib/code_storage_platform/utils.ex
@@ -12,20 +12,13 @@ defmodule CodeStoragePlatform.Utils do
   end
 
   def get_repo_and_user(repo_path) do
-    with raw_response <- Client.get_repo(repo_path),
-         processed_response <- process_response(raw_response),
-         do: {:ok, processed_response}
+    with %{body: raw_body} <- Client.get_repo(repo_path),
+         parsed_body <- Jason.decode!(raw_body),
+         repo_name <- get_in(parsed_body, ["name"]),
+         repo_owner <- get_in(parsed_body, ["owner", "login"]),
+         do: {:ok, %{user: repo_owner, repository: repo_name}}
   end
 
   defp check_if_repo_exists(%{status_code: 200} = _raw_response), do: :ok
   defp check_if_repo_exists(_), do: :error
-
-  def process_response(%{body: raw_body} = _raw_response) do
-    with parsed_body <- Jason.decode!(raw_body),
-         repo_name <- get_in(parsed_body, ["name"]),
-         repo_owner <- get_in(parsed_body, ["owner", "login"]),
-         do: %{user: repo_owner, repository: repo_name}
-  end
-
-  def process_response(_), do: :error
 end
