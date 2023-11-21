2ad0d323afaca2a6d01acb01ff84e428b5ebadb4
Odinei Ribeiro <odinei.ribeiro92@gmail.com>
2023-11-21 01:38:58 -0300

    refatora CodeStoragePlatform.Client

--- lib/code_storage_platform/client.ex
+++ lib/code_storage_platform/client.ex
@@ -1,13 +1,11 @@
 defmodule CodeStoragePlatform.Client do
-  def get_repo(repo_path) do
-    repo_path
-    |> repo_url()
-    |> http_client().get!()
-  end
+  def get_repo(repo_path), do: repo_path |> repo_url() |> http_client().get!()
 
-  defp repo_url(repo_path) do
-    base_url() <> "/repos" <> "/" <> repo_path
-  end
+  def get_repo_issues(repo_path), do: repo_path |> repo_issues_url() |> http_client().get!()
+
+  defp repo_url(repo_path), do: base_url() <> "/repos" <> "/" <> repo_path
+
+  defp repo_issues_url(repo_path), do: base_url() <> "/repos" <> "/" <> repo_path <> "/issues"
 
   defp base_url, do: Application.fetch_env!(:repo_reports, :github)[:base_url]
 
